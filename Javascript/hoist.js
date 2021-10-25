165fdd7f30f3542183912e7726df7130e5824b30
Matt <matthewastro@gmail.com>
2021-10-25 15:51:16 -0400

    Hoist 1st Commit 🚀

---
+++ Javascript/hoist.js
@@ -0,0 +1,77 @@
+var hello = 'world';                                 
+console.log(hello);                                   
+//PRINTS WORLD
+
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+//PRINTS MAGNET
+
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.="support method from-rainbow">log(brendan);
+
+//SYNTAX ERROR
+
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone';
+}
+//HALF-CHICKEN
+
+
+
+
+
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+//TYPE ERROR
+
+
+
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+//UNDEFINED, ROCK, R&B, AND DISCO
+
+
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+
+//SAN JOSE SEATTLE BURBANK SAN JOSE
