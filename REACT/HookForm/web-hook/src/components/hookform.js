019e6f2900a28dfc731674247ccf8e5cf6a19802
Matt <matthewastro@gmail.com>
2021-11-01 00:34:17 -0400

    More webforms

--- REACT/HookForm/web-hook/src/components/hookform.js
+++ REACT/HookForm/web-hook/src/components/hookform.js
@@ -11,18 +11,38 @@ const Form = (props) =>{
         <div className="form-group">
           <label htmlFor="">First Name:</label>
           <input onChange={(e)=>{setfName(e.target.value)}} type="text" name="fName" id="" className="form-control"></input>
+          {
+            fname.length<1 ?
+            <p className = "text-danger">Section must be more than 1 Character!</p>
+          : ""
+          }
         </div>
         <div className="form-group">
           <label htmlFor="">Last Name:</label>
           <input onChange={(e)=>{setlName(e.target.value)}} type="text" name="" id="" className="form-control"></input>
+          {
+            lname.length<1 ?
+            <p className = "text-danger">Section must be more than 1 Character!</p>
+          : ""
+          }
         </div>
         <div className="form-group">
           <label htmlFor="">Email:</label>
           <input onChange={(e)=>{setEmail(e.target.value)}} type="text" name="" id="" className="form-control"></input>
+          {
+            email.length<2 && email.length>0?
+            <p className = "text-danger">Section must be more than 2 Character!</p>
+          : ""
+          }
         </div>
         <div className="form-group">
           <label htmlFor="">Password:</label>
           <input onChange={(e)=>{setPassword(e.target.value)}} type="password" name="" id="" className="form-control"></input>
+          {
+            password.length<1 ?
+            <p className = "text-danger">Section must be more than 1 Character!</p>
+          : ""
+          }
         </div>
         <div className="form-group">
           <label htmlFor="">Confirm Password:</label>
